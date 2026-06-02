e8a778f3b95f8c7d8bc36757471ec5e424b10be6
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-06-01 23:45:34 -0300

    doc - documentando o index

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
 <?php
+    // inicia a sessão para armazenar informações do usuário logado
     session_start();
 
+    // inclui o arquivo de conexão com o banco de dados
     include("infra/db/connect.php");
 
+    // verifica se o formulário de login foi enviado com o método POST
     if($_SERVER['REQUEST_METHOD'] == "POST"){
 
+        // captura os dados do formulário de login (usuário e senha)
         $usuario = $_POST["usuario"];
         $senha = $_POST["senha"];
         
+        // consulta SQL para verificar se o usuário e senha correspondem a um registro na tabela "usuarios"
         $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
+        // executa a consulta SQL e armazena o resultado em uma variável
         $resultado = $conn->query($sql);
 
+        // verifica se encontrou algum usuário na tabela com os dados informados
         if ($resultado->num_rows > 0){
+            // se encontrou um usuário, armazena as informações do usuário na sessão e redireciona para a página "home.php"
             $_SESSION["usuario"] = $usuario;
             header("Location: public/home.php");
             exit();
         }else{
+            // se não encontrou um usuário, define uma mensagem de erro para ser exibida na página de login
             $erro = "Usuário ou senha inválidos!";
         }
     }
@@ -29,8 +38,9 @@
     <title>Login</title>
 </head>
 <body>
-    <h1>Sitema de Login Simples</h1>
+    <h1>Sistema de Login Simples</h1>
 
+    // formulário de login que envia os dados para o mesmo arquivo (index.php) usando o método POST
     <form method="POST">
         <label>Usuário:</label>
         <input type="text" name="usuario">
@@ -39,12 +49,10 @@
         <input type="password" name="senha">
         <br>
         <?php
-        
+            // verifica se a variável de erro foi definida e exibe a mensagem de erro, se houver
             if(isset($erro)){
                 echo $erro;
             };
-
-            // esse erro serve ara alguma coisa
         
         ?>
         <br>
