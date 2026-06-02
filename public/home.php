56d4c5fb67c888c5991616d5be3b31f21530edc9
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-06-01 23:49:50 -0300

    doc - documentando o home

--- public/home.php
+++ public/home.php
@@ -1,22 +1,31 @@
 <?php
+// inicia a sessão para armazenar as informações do usuário logado
 session_start();
+// verifica se o usuário está logado, caso contrário, redireciona para a página de login
 if(!isset($_SESSION["usuario"])){
     header("Location: ../index.php");
     exit();
 }
 
+// inclui o arquivo de conexão com o banco de dados
 include("../infra/db/connect.php");
 
+// verifica se o formulário de cadastro de novo usuário foi enviado com o método POST
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // captura os dados do formulário de cadastro de novo usuário (usuário e senha)
     $novoUsuario = $_POST['usuario'];
     $novaSenha = $_POST['senha'];
 
+    // consulta SQL para inserir um novo usuário na tabela "usuarios"
     $sql = "INSERT INTO usuarios (usuario,senha) 
     VALUES ('$novoUsuario','$novaSenha')";  
 
+    // executa a consulta SQL e verifica se o cadastro foi realizado com sucesso
     if($conn->query($sql) === TRUE){
+        // se o cadastro foi realizado com sucesso, exibe uma mensagem de sucesso
         echo "<script> alert('Usuário cadastrado com sucesso!')</script>";
     }else{
+        // se houve um erro ao realizar o cadastro, exibe uma mensagem de erro
         echo "<script> alert('Erro ao cadastrar')</script>";
     }
 
@@ -32,10 +41,12 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
     <h3>Bem-Vindo! <?php echo $_SESSION["usuario"]; ?></h3>
+    // link para a página de logout, que encerra a sessão do usuário 
     <a href="logout.php"> Sair</a>
 
     <hr>
     <h4>Cadastro de Novo Usuário.</h4>
+    // formulário de cadastro de novo usuário que envia os dados para o mesmo arquivo (home.php) usando o método POST
     <form method="POST">
         <label>Usuário:</label>
         <input type="text" name="usuario">
@@ -44,7 +55,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         <input type="password" name="senha">
         <br>
         <?php
-        
+            // verifica se a variável de erro foi definida e exibe a mensagem de erro, se houver
             if(isset($erro)){
                 echo $erro;
             };
@@ -56,6 +67,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <hr>
     <?php
     
+    // inclui o arquivo "table.php", que exibe uma tabela com os usuários cadastrados no banco de dados
     include("components/table.php")
 
     ?>
