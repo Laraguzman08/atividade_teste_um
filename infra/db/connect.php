3b3016aae2b3a9ee17ce82481e3d1584b9137630
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-06-01 23:59:42 -0300

    doc - documentando connect

--- infra/db/connect.php
+++ infra/db/connect.php
@@ -1,16 +1,21 @@
 <?php
 
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db = "sistema_simples_m1";
+    // configurações de conexão com o banco de dados
+    $host = "localhost"; // endereço do servidor de banco de dados
+    $user = "root"; // nome de usuário do banco de dados
+    $pass = "root"; // senha do banco de dados
+    $db = "sistema_simples_m1"; // nome do banco de dados a ser utilizado
 
+    // cria uma nova conexão com o banco de dados usando as configurações definidas acima e a classe mysqli
     $conn = new mysqli($host,$user,$pass,$db);
 
+    // verifica se houve um erro na conexão, e se sim, exibe uma mensagem de erro e encerra o script. Caso contrário, exibe uma mensagem de sucesso no console do navegador.
     if($conn->connect_error){
         die("Erro na conexão!");
     }else{
+        // a função echo é usada para enviar um script JavaScript para o navegador, que exibe uma mensagem no console do navegador indicando que a conexão com o banco de dados foi bem-sucedida.
         echo "<script>console.log('Banco conectado com sucesso!')</script>";
+        // é recomendado remover ou comentar esta linha em um ambiente de produção para evitar a exposição de informações sensíveis no console do navegador que possam facilitar ataques ao sistema.
     };
 
 ?>
